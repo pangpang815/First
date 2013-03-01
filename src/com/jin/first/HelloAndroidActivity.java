52d5ba28f50b3b124a608c35b322f8e7e53ec740
pangpang815 <pangpang815@gmail.com>
2013-03-02 03:51:24 +0900

    added chapter 3

--- src/com/jin/first/HelloAndroidActivity.java
+++ src/com/jin/first/HelloAndroidActivity.java
@@ -1,26 +1,36 @@
 package com.jin.first;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
-import android.widget.ImageView;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class HelloAndroidActivity extends Activity {
 
+	EditText entry;
+	TextView print;
+	Button okBtn;
+	Button cancelBtn;
+	Button finishBtn;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 //		setContentView(R.layout.main);
 		setContentView(R.layout.linear);
-		
-		TextView textView01 = (TextView)findViewById(R.id.TextView01);
-		String text = textView01.getText().toString();
-		TextView textView02 = (TextView)findViewById(R.id.TextView02);
-		textView02.setText(text);
-		
-		ImageView imageView02 = (ImageView)findViewById(R.id.ImageView02);
-		imageView02.setImageResource(R.drawable.sample_2);
+
+		entry = (EditText) findViewById(R.id.entry);
+		print = (TextView) findViewById(R.id.print);
+		okBtn = (Button) findViewById(R.id.OK);
+		okBtn.setOnClickListener(okListener);
+		cancelBtn = (Button) findViewById(R.id.cancel);
+		cancelBtn.setOnClickListener(cancelListener);
+		finishBtn = (Button) findViewById(R.id.Finish);
+		finishBtn.setOnClickListener(stopListener);
 	}
 
 	@Override
@@ -29,5 +39,30 @@ public class HelloAndroidActivity extends Activity {
 		getMenuInflater().inflate(R.menu.hello_android, menu);
 		return true;
 	}
-
+	
+	OnClickListener okListener = new OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			print.setText(entry.getText());
+		}
+	};
+	
+	OnClickListener cancelListener = new OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			entry.setText("");
+			print.setText("");
+		}
+	};
+	
+	OnClickListener stopListener = new OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			finish();
+		}
+	};
 }
