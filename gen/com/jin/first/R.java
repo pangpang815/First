5d9f824dc58093b99059505f48f5927f311cba23
pangpang815@gmail.com <pangpang815@gmail.com>
2013-03-09 17:31:12 +0900

    added Implicit Intent

--- gen/com/jin/first/R.java
+++ gen/com/jin/first/R.java
@@ -39,49 +39,53 @@ public final class R {
         public static final int watermelon=0x7f020005;
     }
     public static final class id {
-        public static final int OK=0x7f0b0010;
-        public static final int RadioButton01=0x7f0b001a;
-        public static final int RadioButton02=0x7f0b001c;
-        public static final int RadioButton03=0x7f0b001d;
-        public static final int RadioButton04=0x7f0b001e;
-        public static final int RadioButton05=0x7f0b001b;
-        public static final int RadioGroup01=0x7f0b0019;
-        public static final int TextView02=0x7f0b0017;
-        public static final int action_forgot_password=0x7f0b0021;
-        public static final int action_settings=0x7f0b0020;
+        public static final int Finish=0x7f0b000b;
+        public static final int OK=0x7f0b000a;
+        public static final int RadioButton01=0x7f0b001d;
+        public static final int RadioButton02=0x7f0b001f;
+        public static final int RadioButton03=0x7f0b0020;
+        public static final int RadioButton04=0x7f0b0021;
+        public static final int RadioButton05=0x7f0b001e;
+        public static final int RadioGroup01=0x7f0b001c;
+        public static final int TextView02=0x7f0b001a;
+        public static final int action_forgot_password=0x7f0b0024;
+        public static final int action_settings=0x7f0b0023;
         public static final int aplBtn=0x7f0b0000;
+        public static final int browser=0x7f0b0014;
         public static final int button1=0x7f0b0004;
-        public static final int button2=0x7f0b0016;
-        public static final int cancel=0x7f0b000f;
-        public static final int editText1=0x7f0b0015;
-        public static final int email=0x7f0b0007;
-        public static final int entry=0x7f0b000e;
-        public static final int finish=0x7f0b0011;
-        public static final int fontsize=0x7f0b0018;
+        public static final int button2=0x7f0b0019;
+        public static final int cancel=0x7f0b0009;
+        public static final int dial=0x7f0b0013;
+        public static final int editText1=0x7f0b0018;
+        public static final int email=0x7f0b000d;
+        public static final int entry=0x7f0b0008;
+        public static final int finish=0x7f0b0015;
+        public static final int fontsize=0x7f0b001b;
         public static final int imageView1=0x7f0b0006;
         public static final int imgLayer=0x7f0b0003;
-        public static final int label=0x7f0b000d;
-        public static final int login=0x7f0b0009;
-        public static final int next=0x7f0b0012;
-        public static final int notice=0x7f0b000a;
-        public static final int password=0x7f0b0008;
-        public static final int print=0x7f0b0013;
+        public static final int label=0x7f0b0007;
+        public static final int login=0x7f0b000f;
+        public static final int next=0x7f0b0016;
+        public static final int notice=0x7f0b0010;
+        public static final int password=0x7f0b000e;
+        public static final int print=0x7f0b000c;
         public static final int quickContactBadge1=0x7f0b0005;
-        public static final int sign_in_button=0x7f0b000b;
-        public static final int sign_ok=0x7f0b000c;
+        public static final int sign_in_button=0x7f0b0011;
+        public static final int sign_ok=0x7f0b0012;
         public static final int stbBtn=0x7f0b0001;
-        public static final int textView1=0x7f0b0014;
-        public static final int textView2=0x7f0b001f;
+        public static final int textView1=0x7f0b0017;
+        public static final int textView2=0x7f0b0022;
         public static final int wmBtn=0x7f0b0002;
     }
     public static final class layout {
         public static final int activity_image=0x7f030000;
         public static final int framelayout=0x7f030001;
-        public static final int login=0x7f030002;
-        public static final int main=0x7f030003;
-        public static final int relative=0x7f030004;
-        public static final int settings=0x7f030005;
-        public static final int tablelayout=0x7f030006;
+        public static final int linear=0x7f030002;
+        public static final int login=0x7f030003;
+        public static final int main=0x7f030004;
+        public static final int relative=0x7f030005;
+        public static final int settings=0x7f030006;
+        public static final int tablelayout=0x7f030007;
     }
     public static final class menu {
         public static final int hello_android=0x7f0a0000;
@@ -89,65 +93,67 @@ public final class R {
         public static final int login=0x7f0a0002;
     }
     public static final class string {
-        public static final int action_forgot_password=0x7f070016;
+        public static final int action_forgot_password=0x7f070018;
         public static final int action_settings=0x7f070001;
-        public static final int action_sign_in_register=0x7f070014;
-        public static final int action_sign_in_short=0x7f070015;
+        public static final int action_sign_in_register=0x7f070016;
+        public static final int action_sign_in_short=0x7f070017;
         public static final int app_name=0x7f070000;
         public static final int apple=0x7f07000a;
         public static final int banana=0x7f07000b;
+        public static final int btn_browser=0x7f070012;
         public static final int btn_cancel=0x7f070007;
+        public static final int btn_dial=0x7f070011;
         public static final int btn_finish=0x7f070008;
         public static final int btn_next=0x7f070009;
         public static final int btn_ok=0x7f070006;
-        public static final int category=0x7f07002b;
-        public static final int drip=0x7f07002d;
-        public static final int error_field_required=0x7f07001b;
-        public static final int error_incorrect_password=0x7f07001a;
-        public static final int error_invalid_email=0x7f070018;
-        public static final int error_invalid_password=0x7f070019;
+        public static final int category=0x7f07002d;
+        public static final int drip=0x7f07002f;
+        public static final int error_field_required=0x7f07001d;
+        public static final int error_incorrect_password=0x7f07001c;
+        public static final int error_invalid_email=0x7f07001a;
+        public static final int error_invalid_password=0x7f07001b;
         public static final int fontColor=0x7f070010;
         public static final int fontSize=0x7f07000f;
         public static final int hello=0x7f070003;
         public static final int hello_world=0x7f070002;
-        public static final int login_progress_signing_in=0x7f070017;
+        public static final int login_progress_signing_in=0x7f070019;
         public static final int love=0x7f070004;
-        public static final int men=0x7f07002e;
-        public static final int pref_default_display_name=0x7f070021;
-        public static final int pref_description_social_recommendations=0x7f07001f;
+        public static final int men=0x7f070030;
+        public static final int pref_default_display_name=0x7f070023;
+        public static final int pref_description_social_recommendations=0x7f070021;
         /**  Example settings for Data & Sync 
          */
-        public static final int pref_header_data_sync=0x7f070023;
+        public static final int pref_header_data_sync=0x7f070025;
         /**  Strings related to Settings 
  Example General settings 
          */
-        public static final int pref_header_general=0x7f07001d;
+        public static final int pref_header_general=0x7f07001f;
         /**  Example settings for Notifications 
          */
-        public static final int pref_header_notifications=0x7f070026;
-        public static final int pref_ringtone_silent=0x7f070029;
-        public static final int pref_title_add_friends_to_messages=0x7f070022;
-        public static final int pref_title_display_name=0x7f070020;
-        public static final int pref_title_new_message_notifications=0x7f070027;
-        public static final int pref_title_ringtone=0x7f070028;
-        public static final int pref_title_social_recommendations=0x7f07001e;
-        public static final int pref_title_sync_frequency=0x7f070024;
-        public static final int pref_title_system_sync_settings=0x7f070025;
-        public static final int pref_title_vibrate=0x7f07002a;
+        public static final int pref_header_notifications=0x7f070028;
+        public static final int pref_ringtone_silent=0x7f07002b;
+        public static final int pref_title_add_friends_to_messages=0x7f070024;
+        public static final int pref_title_display_name=0x7f070022;
+        public static final int pref_title_new_message_notifications=0x7f070029;
+        public static final int pref_title_ringtone=0x7f07002a;
+        public static final int pref_title_social_recommendations=0x7f070020;
+        public static final int pref_title_sync_frequency=0x7f070026;
+        public static final int pref_title_system_sync_settings=0x7f070027;
+        public static final int pref_title_vibrate=0x7f07002c;
         /**  Strings related to login 
          */
-        public static final int prompt_email=0x7f070012;
-        public static final int prompt_password=0x7f070013;
-        public static final int receive=0x7f070030;
-        public static final int reject=0x7f070031;
-        public static final int roasting=0x7f07002c;
+        public static final int prompt_email=0x7f070014;
+        public static final int prompt_password=0x7f070015;
+        public static final int receive=0x7f070032;
+        public static final int reject=0x7f070033;
+        public static final int roasting=0x7f07002e;
         public static final int strawberry=0x7f07000c;
         public static final int title_activity_image=0x7f07000e;
-        public static final int title_activity_login=0x7f070011;
-        public static final int title_activity_settings=0x7f07001c;
+        public static final int title_activity_login=0x7f070013;
+        public static final int title_activity_settings=0x7f07001e;
         public static final int type1=0x7f070005;
         public static final int watermelon=0x7f07000d;
-        public static final int women=0x7f07002f;
+        public static final int women=0x7f070031;
     }
     public static final class style {
         /** 
--- src/com/jin/first/sample/LoginActivity.java
+++ src/com/jin/first/sample/LoginActivity.java
@@ -1,6 +1,8 @@
 package com.jin.first.sample;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -10,7 +12,12 @@ import com.jin.first.R;
 
 public class LoginActivity extends Activity {
 
+	static final int REQ_DIAL = 1;
+	static final int REQ_BROWSER = 1;
+	
 	Button okBtn;
+	Button dialBtn;
+	Button browserBtn;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +25,10 @@ public class LoginActivity extends Activity {
 		setContentView(R.layout.login);
 		
 		okBtn = (Button) findViewById(R.id.sign_ok);
-		okBtn.setOnClickListener(okListener);		
+		okBtn.setOnClickListener(okListener);
+		
+		findViewById(R.id.dial).setOnClickListener(intentListener);
+		findViewById(R.id.browser).setOnClickListener(intentListener);
 	}
 	
 	OnClickListener okListener = new OnClickListener() {
@@ -32,4 +42,35 @@ public class LoginActivity extends Activity {
 			}
 		}
 	};
+	
+	OnClickListener intentListener = new OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			Intent intent;
+			switch (v.getId()) {
+				case R.id.dial:
+					//액티비티 전환을 위한 인텐트
+					intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:0123456789"));	//작업 지시서(인텐스)
+					
+					//요구 코드: 되돌아 왔을 때 어느 액티비티를 수행할 것인지 근거 마련
+					startActivityForResult(intent, REQ_DIAL);
+					break;
+				case R.id.browser:
+					intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.naver.com"));
+					startActivityForResult(intent, REQ_BROWSER);
+					break;
+			}
+		}
+	};
+	
+	/**
+	 * 내가 호출한 액티비티가 종료되면 다시 돌아온다
+	 * 
+	 * @see android.app.Activity#onActivityResult(int, int, android.content.Intent)
+	 */
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		setTitle(String.valueOf(resultCode));
+	}
 }
